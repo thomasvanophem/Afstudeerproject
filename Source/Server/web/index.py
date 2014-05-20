cbe2571675a71643f253cbadc892091c298352d3
Thomas van Ophem <thomas.vanophem@gmail.com>
2014-05-20 17:00:15 +0200

    Added google maps API

--- Source/Server/web/index.py
+++ Source/Server/web/index.py
@@ -6,14 +6,15 @@ class Index():
     @cherrypy.expose
     def index(self, lat = None, lon = None, radius = None):
         if cherrypy.request.method == "GET":
-            return interface("Home", """<h2>Welkom!</h2>
-                                        <form method="POST" action="">
-                                        Latitude: <input type="text" name="lat" id="lat" /><br />
-                                        Longitude: <input type="text" name="lon" id="lon" /><br />
-                                        Radius in KM: <input type="text" name="radius" id="radius" /><br />
-                                        <input type="submit" value="Verzenden" />
-                                        </form>
-                                     """)
+            #return interface("Home", """<h2>Welkom!</h2>
+            #                            <form method="POST" action="">
+            #                            Latitude: <input type="text" name="lat" id="lat" /><br />
+            #                            Longitude: <input type="text" name="lon" id="lon" /><br />
+            #                            Radius in KM: <input type="text" name="radius" id="radius" /><br />
+            #                            <input type="submit" value="Verzenden" />
+            #                            </form>
+            #                        """)
+            return interface("Home", "<div id="map-canvas"></div>")
         elif cherrypy.request.method == "POST":
             temp = coordinate_calc.main((lat, lon), radius)
             return interface("Home", """<h2>Welkom!</h2>
