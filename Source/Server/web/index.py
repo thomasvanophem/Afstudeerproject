85da7bd9c840ab70da2d76b22beed9d394610131
Thomas van Ophem <thomas.vanophem@gmail.com>
2014-05-25 19:41:48 +0200

    Added recursive city search

--- Source/Server/web/index.py
+++ Source/Server/web/index.py
@@ -6,26 +6,8 @@ class Index():
     @cherrypy.expose
     def index(self, lat = None, lon = None, radius = None):
         if cherrypy.request.method == "GET":
-            #return interface("Home", """<h2>Welkom!</h2>
-            #                            <form method="POST" action="">
-            #                            Latitude: <input type="text" name="lat" id="lat" /><br />
-            #                            Longitude: <input type="text" name="lon" id="lon" /><br />
-            #                            Radius in KM: <input type="text" name="radius" id="radius" /><br />
-            #                            <input type="submit" value="Verzenden" />
-            #                            </form>
-            #                        """)
             return interface("Home", "")
         elif cherrypy.request.method == "POST":
-            temp = coordinate_calc.main((lat, lon), radius)
-            print json.dumps(temp)
+            temp = coordinate_calc.main((lat, lon), radius, "../Download/cities15000.txt", [])
+
             return json.dumps(temp)
-	    #return return_results("""Latitude: %s <br />
-            #                        Longitude: %s <br />
-            #                        Radius: %s <br />
-            #                        Cities: %s"""%(float(lat), float(lon), float(radius), temp))
-            #return interface("Home", """<h2>Welkom!</h2>
-            #                            Latitude: %s <br />
-            #                            Longitude: %s <br />
-            #                            Radius: %s <br/>
-            #                            Steden: %s
-            #                         """%(float(lat), float(lon), float(radius), temp))
