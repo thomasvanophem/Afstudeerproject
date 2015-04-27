c6b5b6d89d7c72373678240baa9811cc6193d4d1
thomasvanophem <thomas.vanophem@gmail.com>
2015-04-27 14:00:29 +0200

    Create coordinate_calc.py
    new coordinate_calc.py

---
+++ Source/Server/coordinate_calc.py
@@ -0,0 +1,49 @@
+"""
+Author: Thomas van Ophem thomas.vanophem@student.uva.nl
+Date: 30-04-2014
+"""
+
+import math
+import glob
+
+import database
+
+def get_cities(db_name, r, lat, lon):
+    result = []
+    print r
+    dlon = math.asin(math.sin(r)/math.cos(lat))
+    
+    lat_min = str(lat - r)
+    lat_max = str(lat + r)
+    lon_min = str(lon - dlon)
+    lon_max = str(lon + dlon)
+    
+    fields = ["NAME"]
+    where = "(LAT >= " + lat_min + " AND  LAT <= " + lat_max + \
+            ") AND (LON >= " + lon_min + " AND LON <= " + lon_max + ")" + \
+            " GROUP BY NAME HAVING acos(sin(" + str(lat) + ") * sin(LAT) + cos(" + \
+            str(lat) + ") * cos(LAT) * cos(LON - (" + str(lon) + "))) <= " + str(r)
+    
+    print where
+    
+    db = database.Database(db_name)
+    
+    for row in db.select("cities", fields, where):
+        result.append(row)
+    
+    db.close()
+    
+    return result    
+    
+def main(db, source, radius): #, db, result):
+    r = float(radius) / 6371
+    lat = math.radians(float(source[0]))
+    lon = math.radians(float(source[1]))
+    cities = get_cities(db, r, lat, lon)
+    
+    print cities
+    
+    return cities
+    
+if __name__ == "__main__":
+    main("geo_data.db", (52.6333333, 4.75), 50)
