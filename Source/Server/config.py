af9724a06dde9f2c261e09793f9f6a4d8955b14a
thomasvanophem <thomas.vanophem@gmail.com>
2015-04-27 13:59:41 +0200

    Create config.py
    new config.py

---
+++ Source/Server/config.py
@@ -0,0 +1,17 @@
+"""
+Config file for the server and download script.
+
+Author: Thomas van Ophem, thomas.vanophem@student.uva.nl
+Date: 29-04-2014
+"""
+
+# Files to download from the geonames database (database dumps).
+download_files = ["http://download.geonames.org/export/dump/cities1000.zip"]
+                    
+# Color codes to print fancy info/warning/error messages.     
+colors = {'BUSY' : '\033[1;47m', 'INFO' : '\033[1;46m', 'OK' : '\033[1;42m', 
+            'WARNING' : '\033[1;43m', 'ERROR' : '\033[1;41m', 
+            'END' : '\033[1;m'}
+            
+log = True
+to_file = False
