0af517271ad15a5e2aaadcbad0774aafadd1955a
Thomas van Ophem <thomas.vanophem@gmail.com>
2015-04-30 17:13:30 +0200

    server now uses static dirs and files for css

--- Source/Server/server.py
+++ Source/Server/server.py
@@ -8,7 +8,13 @@ def main():
 			'tools.sessions.on': True,
 			'tools.sessions.timeout': 20160, #14 days
 			'server.socket_host': '192.168.0.13',
-			'tools.trailing_slash.on': False}
+			'tools.trailing_slash.on': False},
+                    '/css': {
+                        'tools.staticdir.on': True,
+                        'tools.staticdir.dir': "/home/thomas/Afstudeerproject/Source/Server/webdata/css"},
+                    '/layout.css': {
+                        'tools.staticfile.on': True,
+                        'tools.staticfile.filename': "/home/thomas/Afstudeerproject/Source/Server/webdata/css/layout.css"}
                     }
 
     cherrypy.quickstart(Index(), config = cherry_conf)
