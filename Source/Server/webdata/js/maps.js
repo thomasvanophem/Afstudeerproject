7681bff6daf59d73b9bccecd95b90ded3330602c
Thomas van Ophem <thomas.vanophem@gmail.com>
2015-05-16 17:22:00 +0200

    research

--- Source/Server/webdata/js/maps.js
+++ Source/Server/webdata/js/maps.js
@@ -160,6 +160,23 @@ function initialize() {
 
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+    var selection
+    var selectionOptions = {
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: new google.maps.LatLng(52.0833333, 5.1333333),
+      radius: 25.0 * 1000.0
+    };
+    // Add the circle for this city to the map.
+    selection = new google.maps.Circle(selectionOptions);
+    console.log("Radius: "+selection.radius+" KM");
+    console.log("Lat: "+selection.lat()+" Lon: "+center.lng());
+    loadXMLDoc(selection.radius, selection.lat(), selection.lng());
+
     var drawingManager = new google.maps.drawing.DrawingManager({
                                                                 drawingMode: google.maps.drawing.OverlayType.CIRCLE,
                                                                 drawingControl: false,
